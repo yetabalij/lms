7b3b92d723614bf490ab4d3249572a024e730962
ermiastsegu <ermiastsegu@yahoo.com>
2024-03-11 14:55:50 +0300

    middleware used and testing routes are created.

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,27 @@
-import express from "express";
+require("dotenv").config();
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 export const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+//testing api
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "api is working",
+  });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = `Resource not found.`;
+  next(err);
+});
