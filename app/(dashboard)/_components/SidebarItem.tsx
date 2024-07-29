75bd103b43e9eec7a8d6af8b185b367daef45fb5
ermiastsegu <ermiastsegu@yahoo.com>
2024-07-29 16:35:41 +0300

    side bar developed.

---
+++ app/(dashboard)/_components/SidebarItem.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+interface SidebarItemProps {
+  icon: LucideIcon;
+  label: string;
+  href: string;
+}
+
+const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
+  const pathname = usePathname(); // Call the hook to get the current pathname
+  const router = useRouter();
+
+  const isActive =
+    (pathname === "/" && href === "/") ||
+    pathname === href ||
+    pathname?.startsWith(`${href}/`);
+
+  const onClick = () => {
+    router.push(href);
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      className={cn(
+        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        isActive &&
+          "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+      )}
+    >
+      <Icon
+        size={22}
+        className={cn("text-slate-500", isActive && "text-sky-700")}
+      />
+      {label}
+    </button>
+  );
+};
+
+export default SidebarItem;
