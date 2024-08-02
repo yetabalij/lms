141ed5cab28ad96cfe1127092faf2b2cf9a29fba
ermiastsegu <ermiastsegu@yahoo.com>
2024-08-02 15:03:28 +0300

    navebar route is started.

---
+++ components/NavbarRoutes.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const NavbarRoutes = () => {
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton />
+    </div>
+  );
+};
+
+export default NavbarRoutes;
